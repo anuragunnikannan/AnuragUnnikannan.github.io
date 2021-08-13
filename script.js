082f64797ab07b08c2ac59ffd497d7c5cd9792ca
AnuragUnnikannan <anuragunnikannan7@gmail.com>
2021-08-13 23:33:42 +0530

    Optimized for mobile screens

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ function dispHamburger()
 {
     var x = document.getElementsByTagName("header");
     var y = document.getElementsByTagName("section");
-    if(x[0].style.display == "block")
+    if(x[0].style.position == "fixed")
     {
-        x[0].style.display = "none";
-        x[0].style.visibility = "hidden";
-        /* for(var i = 0;i < y.length;i++)
+        x[0].style.position = "absolute";
+        x[0].style.left = "-100rem";
+
+        for(var i = 0;i < y.length;i++)
         {
-            y[i].classList.remove("blur_effect");
-        } */
+            y[i].style.display.marginLeft("5rem");
+        }
     }
     else
     {
-        x[0].style.display = "block";
-        x[0].style.visibility = "visible";
-        /* for(var i = 0;i < y.length;i++)
+        x[0].style.position = "fixed";
+        x[0].style.left = "0";
+        for(var i = 0;i < y.length;i++)
         {
-            y[i].classList.add("blur_effect");
+            y[i].style.display.marginLeft("38rem");
         }
-        x[0].style.opacity = "1"; */
+        /*x[0].style.opacity = "1"; */
         /* x[0].style.boxShadow = "50rem 50rem 100rem black"; */
         
     }
