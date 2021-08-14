fb7130f757dcf75be5021e0e2c83516a721b0409
AnuragUnnikannan <anuragunnikannan7@gmail.com>
2021-08-15 02:56:11 +0530

    Fixed retainHeader() crashing & 2 more functions

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
-function dispHamburger()
+function showHeader()
 {
     var x = document.getElementsByTagName("header");
-    var y = document.getElementsByTagName("section");
-    if(x[0].style.position == "fixed")
+    var y = document.getElementsByTagName("main");
+    x[0].style.position = "fixed";
+    x[0].style.left = "0";
+    y[0].classList.add("blur_effect");
+}
+
+function hideHeader()
+{
+    var x = document.getElementsByTagName("header");
+    var y = document.getElementsByTagName("main");
+    var z = window.outerWidth;
+    if(z < 980)
     {
         x[0].style.position = "absolute";
         x[0].style.left = "-100rem";
-
-        for(var i = 0;i < y.length;i++)
-        {
-            y[i].classList.remove("blur_effect");
-        }
-    }
-    else
-    {
-        x[0].style.position = "fixed";
-        x[0].style.left = "0";
-        for(var i = 0;i < y.length;i++)
-        {
-            y[i].classList.add("blur_effect");
-        }
+        y[0].classList.remove("blur_effect");
     }
 }
 
-window.addEventListener("resize", retain_header);
-
-function retain_header()
+/* For displaying/hiding header on clicking hamburger menu. For screens having width less than 980px */
+function dispHeader()
 {
     var x = document.getElementsByTagName("header");
-    var y = window.outerWidth;
-    if(y > 980)
+
+    /* If hamburger menu is visible, then hide it, else show. */
+    if(x[0].style.position == "fixed")
     {
-        x[0].style.position = "fixed";
-        x[0].style.left = "0";
+       hideHeader();
     }
     else
     {
-        x[0].style.position = "absolute";
-        x[0].style.left = "-100rem";
+        showHeader();
+    }
+}
+
+/* Show header when window is larger than 980px and hide it when less than 980px */
+window.addEventListener("resize", retainHeader);
+function retainHeader()
+{
+    var y = window.outerWidth;
+    if(y > 980)
+    {
+        showHeader();
+    }
+    else
+    {
+        hideHeader();
     }
 }
